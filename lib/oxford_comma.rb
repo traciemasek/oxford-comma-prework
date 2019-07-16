9b17cb50d0b9114781142460ff32cb546098f0b2
Tracie Masek <traciemasek@gmail.com>
2019-07-16 10:42:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    return array[0]
+  elsif array.size == 2 
+    return array.join(" and ")
+  else
+   last_el = array.pop
+   return "#{array.join(", ")}, and #{last_el}"
+  end
 end
