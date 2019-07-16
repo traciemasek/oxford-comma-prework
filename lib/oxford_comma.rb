0160db099f60de81d3b5d683eb495941d29ac8d9
Tracie Masek <traciemasek@gmail.com>
2019-07-16 10:39:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   last_el = array.pop
   if array.length == 1 
-    array.to_s
+    return array.to_s
   elsif array.length == 2 
-    array.join(" and ")
+    return array.join(" and ")
   else
-   "#{array.join(", ")}, and #{last_el}"
+   return "#{array.join(", ")}, and #{last_el}"
   end
 end
